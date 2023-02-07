7d27da2560aba37664dcbd642645881b03d3fad1
rbzf <anna.sadkowska@gmail.com>
2023-02-07 17:07:29 +0100

    drugi commit

--- src/main/java/pl/javastart/task/Book.java
+++ src/main/java/pl/javastart/task/Book.java
@@ -4,4 +4,10 @@ public class Book {
     String title;
     String author;
     String publisher;
+
+    public Book(String t, String a, String p){
+        title = t;
+        author = a;
+        publisher = p;
+    }
 }
--- src/main/java/pl/javastart/task/BookShelf.java
+++ src/main/java/pl/javastart/task/BookShelf.java
@@ -3,15 +3,8 @@ package pl.javastart.task;
 public class BookShelf {
 
     public static void main(String[] args) {
-        Book book1 = new Book();
-        book1.title = "Prawdziwa Odessa";
-        book1.author = "Uki Goni";
-        book1.publisher = "Replika";
-
-        Book book2 = new Book();
-        book2.title = "Nelka i piesek Fafik";
-        book2.author = "Dorota Loskot-Cichocka";
-        book2.publisher = "Edukacyjny Egmont";
+        Book book1 = new Book("Prawdziwa Odessa", "Uki Goni", "Replika");
+        Book book2 = new Book("Nelka i piesek Fafik", "Dorota Loskot-Cichocka", "Edukacyjny Egmont");
 
         System.out.printf("tytul: %s, autor: %s, wydawnictwo: %s%n", book1.title, book1.author, book1.publisher);
         System.out.printf("tytul: %s, autor: %s, wydawnictwo: %s%n", book2.title, book2.author, book2.publisher);
